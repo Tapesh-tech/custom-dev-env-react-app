1146098d129fe6df2b8b01d3f1fbd1431b3b777d
Bhandari, Tapesh <tbhandari@cvent.com>
2021-01-10 14:00:31 +0530

    Basic Env Setup

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import { render } from "enzyme";
+import React from "react";
+import { render } from "react-dom";
+
+function Hi() {
+  return (
+    <>
+      <p>Yo!!!!!</p>
+    </>
+  );
+}
+
+render(<Hi />, document.querySelector("#myApp"));
